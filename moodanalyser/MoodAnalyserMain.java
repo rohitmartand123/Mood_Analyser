2c979b8d51f4f2239519fd50a8dcdcda1652db16
rohitmartand123 <rohitmartand274@gmail.com>
2022-05-11 22:04:45 +0530

    Analyse mood wheather it is happy or sad

--- moodanalyser/MoodAnalyserMain.java
+++ moodanalyser/MoodAnalyserMain.java
@@ -7,16 +7,16 @@ package com.bridegelabz.moodanalyser;
  */
 
 public class MoodAnalyserMain {
-    public static void main(String[] args) {
-        System.out.println("-------- Welcome To Mood Analyser Program --------");
+        public static void main(String[] args) {
+            System.out.println("-------- Welcome To Mood Analyser Program --------");
 
-        // Create object
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
+            // Create object
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
 
-        // Pass Happy message
-        System.out.println(moodAnalyser.analyseMood("I am in happy Mood"));
+            // Pass Happy message
+            System.out.println(moodAnalyser.analyseMood("I am in happy Mood"));
 
-        // Pass Sad message
-        System.out.println(moodAnalyser.analyseMood("I am in Sad Mood"));
+            // Pass Sad message
+            System.out.println(moodAnalyser.analyseMood("I am in Sad Mood"));
     }
     }
