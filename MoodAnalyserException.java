42f8e12c472087fa63d4f901e9a69851e36a4a07
rohitmartand123 <rohitmartand274@gmail.com>
2022-05-11 22:24:55 +0530

    Program to check custom exception

---
+++ MoodAnalyserException.java
@@ -0,0 +1,2 @@
+package com.bridegelabz;public class MoodAnalyserException {
+}
--- moodanalyser/MoodAnalyserMain.java
+++ moodanalyser/MoodAnalyserMain.java
@@ -9,12 +9,13 @@ package com.bridegelabz.moodanalyser;
 public class MoodAnalyserMain {
         public static void main(String[] args) {
             System.out.println("-------- Welcome To Mood Analyser Program --------");
+            System.out.println();
 
             // Create object
             MoodAnalyser moodAnalyser = new MoodAnalyser();
 
             // Pass Happy message
-            System.out.println(moodAnalyser.analyseMood("I am in happy Mood"));
+            System.out.println(moodAnalyser.analyseMood("My name is Snehal"));
 
             // Pass Sad message
             System.out.println(moodAnalyser.analyseMood("I am in Sad Mood"));
